c471aeaefaf153790518da1513fba87f7cdc7916
Steven Barnhurst <stevenbarnhurst@gmail.com>
2016-09-13 20:40:38 -0600

    feat: add fn to home controller

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,7 +5,30 @@
     .module('core')
     .controller('HomeController', HomeController);
 
-  function HomeController() {
+  HomeController.$inject = ['$scope', '$state', '$http'];
+
+  function HomeController($scope, $state, $http) {
+
     var vm = this;
+
+    vm.from = '';
+    vm.to = '';
+    vm.isRoute = false;
+
+    var url = '/api/routes';
+
+    vm.getRoute = function () {
+      $http.get({
+        url: url,
+        method: 'GET',
+        params: {from: vm.from, to: vm.to}
+      }).then(function (data) {
+        vm.results = data.data;
+        vm.isRoute = true;
+      }, function (error) {
+        console.log(error);
+      });
+    }
+
   }
 }());
--- modules/rtd/client/services/rtd.client.service.js
+++ modules/rtd/client/services/rtd.client.service.js
@@ -1,53 +1,53 @@
-(function () {
-  'use strict';
-
-  angular
-    .module('rtd.services')
-    .factory('RtdService', RtdService);
-
-  RtdService.$inject = ['$resource'];
-
-  function RtdService($resource) {
-    var Article = $resource('api/rtd/:articleId', {
-      articleId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      }
-    });
-
-    angular.extend(Article.prototype, {
-      createOrUpdate: function () {
-        var article = this;
-        return createOrUpdate(article);
-      }
-    });
-
-    return Article;
-
-    function createOrUpdate(article) {
-      if (article._id) {
-        return article.$update(onSuccess, onError);
-      } else {
-        return article.$save(onSuccess, onError);
-      }
-
-      // Handle successful response
-      function onSuccess(article) {
-        // Any required internal processing from inside the service, goes here.
-      }
-
-      // Handle error response
-      function onError(errorResponse) {
-        var error = errorResponse.data;
-        // Handle error internally
-        handleError(error);
-      }
-    }
-
-    function handleError(error) {
-      // Log error
-      console.log(error);
-    }
-  }
-}());
+// (function () {
+//   'use strict';
+//
+//   angular
+//     .module('rtd.services')
+//     .factory('RtdService', RtdService);
+//
+//   RtdService.$inject = ['$resource'];
+//
+//   function RtdService($resource) {
+//     var rtd = $resource('api/rtd/', {
+//       articleId: '@_id'
+//     }, {
+//       update: {
+//         method: 'PUT'
+//       }
+//     });
+//
+//     angular.extend(Article.prototype, {
+//       createOrUpdate: function () {
+//         var article = this;
+//         return createOrUpdate(article);
+//       }
+//     });
+//
+//     return Article;
+//
+//     function createOrUpdate(article) {
+//       if (article._id) {
+//         return article.$update(onSuccess, onError);
+//       } else {
+//         return article.$save(onSuccess, onError);
+//       }
+//
+//       // Handle successful response
+//       function onSuccess(article) {
+//         // Any required internal processing from inside the service, goes here.
+//       }
+//
+//       // Handle error response
+//       function onError(errorResponse) {
+//         var error = errorResponse.data;
+//         // Handle error internally
+//         handleError(error);
+//       }
+//     }
+//
+//     function handleError(error) {
+//       // Log error
+//       console.log(error);
+//     }
+//   }
+// }());
